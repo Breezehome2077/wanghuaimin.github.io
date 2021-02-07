a31e920d3880de0a0581f8aba02050debb0326e7
hetieliancheng <hetieliancheng@outlook.com>
2021-02-07 11:09:06 +0800

    positon fixed doudong 8

--- assets/js/wzm.js
+++ assets/js/wzm.js
@@ -1,4 +1,15 @@
 (function($){
     console.log('人对了，世界就对了。这一句话个人认为有两种解释：第一种是“改变自己，认识世界”，第二种是“认识自己，改变世界”。愿人人都能通过第二种获得幸福生活。');
-    $("#site-content").find("a").attr('target','_blank')
-})(jQuery);
+    let p=0,t=0;
+    $(window).scroll(
+        function(){
+            p = $(this).scrollTop();
+            if (t<=p) {
+                $('.site-aside-wrap').css({'bottom':'-40px'});
+            }
+            else {
+                $('.site-aside-wrap').css({'bottom':'0'});
+            }
+        }
+    );
+})();
