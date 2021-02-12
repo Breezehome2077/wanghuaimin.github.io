e4430e8ae23f22e525dd5a7159c3be244cebb66b
hetieliancheng <hetieliancheng@outlook.com>
2021-02-12 11:15:00 +0800

    fixed 40

--- assets/js/wzm.js
+++ assets/js/wzm.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-    // 当DIV使用fixed定位时，在手机上滑动页面，该DIV会不停抖动，
-    // 所以这里使用jq将DIV在抖动方向上进行隐藏。
+    // fixed 抖动的解决办法
     let screen_w=window.innerWidth;
     if (screen_w <= 768) {
         let p=0,t=0;
         $(window).scroll(function(){
             p = $(this).scrollTop();
             if(t<=p){//向下滚
-                $('.site-aside-wrap').css({'position':'fixed','bottom':'-40px','display':'none'});
+                $('.site-aside').css({'transform':'translateY(-40px)','display':'none'});
             }else{//向上滚
-                $('.site-aside-wrap').css({'position':'fixed','bottom':'0px','display':'block'});
+                $('.site-aside').css({'transform':'translateY(0px)','display':'block'});
             }
             setTimeout(function(){t = p;},0);
         });
