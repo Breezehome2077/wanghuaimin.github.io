5593ae20f494d585ab8e195d6c30182ee643b680
hetieliancheng <hetieliancheng@outlook.com>
2021-02-13 11:13:20 +0800

    fixed 1302

--- assets/js/wzm.js
+++ assets/js/wzm.js
@@ -1,22 +1,6 @@
 $(document).ready(function(){
-    // fixed 抖动的解决办法
-    let screen_w=window.innerWidth;
-    if (screen_w <= 768) {
-        let p=0,t=0;
-        $(window).scroll(function(){
-            p = $(this).scrollTop();
-            if(t<=p){//向下滚
-                $('.site-aside-wrap').addClass('wzm-fixed-down');
-                if($('.site-aside-wrap').hasClass('wzm-fixed-up')){
-                    $('.site-aside-wrap').removeClass('wzm-fixed-up');
-                }
-            }else{//向上滚
-                $('.site-aside-wrap').addClass('wzm-fixed-up');
-                if($('.site-aside-wrap').hasClass('wzm-fixed-down')){
-                    $('.site-aside-wrap').removeClass('wzm-fixed-down');
-                }
-            }
-            setTimeout(function(){t = p;},0);
-        });
-    }
+    // fixed 抖动的解决办法 - 禁用手机浏览器的滑动回弹效果
+    document.addEventListener('touchmove',function(ev){
+        ev.preventDefault();
+    },{passive:false});
 });
